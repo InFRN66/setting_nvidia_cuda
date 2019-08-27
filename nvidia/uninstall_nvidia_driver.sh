41eadc7699cfea83ab41a2071f4119295fd11bc9
Hosoya Yusuke <yhosoya@HosoyanoMacBook-Pro.local>
2019-08-27 22:05:43 +0900

    first commit

---
+++ nvidia/uninstall_nvidia_driver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# in case installed with run file : 
+cd ~/Donwloads
+sudo ./NVIDIA-Linux-~~~.run --uninstall
+sudo reboot now
+
+cd ~/Donwloads
+sudo ./NVIDIA-Linux-~~~.run
+sudo reboot now
